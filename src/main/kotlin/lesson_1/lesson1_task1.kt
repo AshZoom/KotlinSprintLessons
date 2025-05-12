ac6bd70a01c2e48de4cc11ba3e1935a2cf00ee87
Practicum <practicum@ya.ru>
2025-05-12 19:16:47 +0300

    KS-1-1 create file 'lesson1_task1-3.kt'

--- src/main/kotlin/lesson_1/lesson1_task1.kt
+++ src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -1,5 +1,30 @@
 package org.example.lesson_1
 
 fun main() {
-    println("Решение к уроку 1 Задание 1")
+    //Задача 1
+    var numbersOfOrders=75
+    val thankfulMessage="Thank you for your order."
+
+    //Задача 2
+    println (numbersOfOrders)
+    println(thankfulMessage)
+    var numberOfEmployee=2000
+    println (numberOfEmployee)
+
+    numberOfEmployee--
+    println (numberOfEmployee)
+
+    //Задача 3
+    val year=1961
+    var hour=9
+    var minute=7
+    println(year)
+    println("Взлёт:")
+    println("Часы"+" %02d".format(hour))
+    println("Минуты"+" %02d".format(minute))
+
+    hour=10
+    minute=55
+    println("Посадка:")
+    println("%02d:%02d".format(hour,minute))
 }
