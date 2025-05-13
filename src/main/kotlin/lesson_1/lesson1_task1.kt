a0b7c39ddc5feff4f0a52f71a63132b403996c0f
Practicum <practicum@ya.ru>
2025-05-13 21:35:15 +0300

    KS-1-1 task1 solution

--- src/main/kotlin/lesson_1/lesson1_task1.kt
+++ src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -2,49 +2,8 @@ package org.example.lesson_1
 
 fun main() {
     //Задача 1
-    var numbersOfOrders=75
-    val thankfulMessage="Thank you for your order."
-
-    //Задача 2
-    println (numbersOfOrders)
-    println(thankfulMessage)
-    var numberOfEmployee=2000
-    println (numberOfEmployee)
-
-    numberOfEmployee--
-    println (numberOfEmployee)
-
-    //Задача 3
-    val year=1961
-    var hour=9
-    var minute=7
-    println(year)
-    println("Взлёт:")
-    println("Часы"+" %02d".format(hour))
-    println("Минуты"+" %02d".format(minute))
-
-    hour=10
-    minute=55
-    println("Посадка:")
-    println("%02d:%02d".format(hour,minute))
-
-    //Задача 4
-    val distance: Long =40868600000
-    val age:Byte=27
-    val partOfDay:Float=0.075f
-    val seconds:Short=6480
-    val partOfYear:Double= 2.0547945220547944E-4
-    val orbit:Int=327000
-    println("$distance $age $partOfDay  $seconds $partOfYear $orbit")
-
-
-    //Задача 5
-    val secondsTotal=6480
-    val hoursInSpace=secondsTotal/3600
-    val minutsInSpace=(secondsTotal % 3600)/60
-    val secondsInSpace = (secondsTotal % 3600) % 60
-    println("%02d:%02d:%02d".format(hoursInSpace,minutsInSpace,secondsInSpace))
-
+    var numbersOfOrders = 75
+    val thankfulMessage = "Thank you for your order."
 
 
 }
