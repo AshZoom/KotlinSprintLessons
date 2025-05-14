07ba9a17f115fe3e1a10e98031435b76eace53f2
Practicum <practicum@ya.ru>
2025-05-14 16:33:03 +0300

    KS-1-5 task5 bugs removed

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -2,9 +2,14 @@ package org.example.lesson_1
 
 fun main() {
     //Задача 5
+
+    val SECONDS_IN_HOUR = 3600
+    val MINUTES_IN_HOUR=60
+    val SECONDS_IN_MINUTE=60
     val secondsTotal = 6480
-    val hoursInSpace = secondsTotal / 3600
-    val minutsInSpace = (secondsTotal % 3600) / 60
-    val secondsInSpace = (secondsTotal % 3600) % 60
+    val restOfSeconds = secondsTotal % SECONDS_IN_HOUR
+    val hoursInSpace = secondsTotal / SECONDS_IN_HOUR
+    val minutsInSpace = restOfSeconds / MINUTES_IN_HOUR
+    val secondsInSpace = restOfSeconds % SECONDS_IN_MINUTE
     println("%02d:%02d:%02d".format(hoursInSpace, minutsInSpace, secondsInSpace))
 }
